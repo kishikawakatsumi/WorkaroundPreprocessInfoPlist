f9bf10bccfd3b395d7c883ec83afcf65a6c05d8d
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2018-09-25 08:38:34 +0900

    First commit

---
+++ WorkaroundPreprocessInfoPlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkaroundPreprocessInfoPlist
+//
+//  Created by Kishikawa Katsumi on 2018/09/25.
+//  Copyright © 2018 Kishikawa Katsumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
